819d824b0a4f5f98f03c99431ef7829cd3da1ce0
JRepo42 <JRepo42@users.noreply.github.com>
2024-04-07 20:05:34 +0100

    add(config damage integration)

--- src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
+++ src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
@@ -1,9 +1,13 @@
 package elocindev.shield_overhaul.entity;
 
+import elocindev.shield_overhaul.ShieldOverhaul;
+import elocindev.shield_overhaul.config.ConfigEntries;
 import elocindev.shield_overhaul.registry.EffectRegistry;
 import elocindev.shield_overhaul.registry.EntityRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
@@ -16,6 +20,7 @@ import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
 public class ShieldBashEntity extends PersistentProjectileEntity {
+    ConfigEntries config = ShieldOverhaul.CONFIG;
     private int life = 0;
 
     public ShieldBashEntity(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
@@ -25,7 +30,7 @@ public class ShieldBashEntity extends PersistentProjectileEntity {
     public ShieldBashEntity(LivingEntity owner, World world) {
         super(EntityRegistry.SHIELD_BASH_ENTITY, owner, world);
         this.setNoGravity(true);
-        this.setDamage(0f);
+        this.setDamage(config.bash_damage);
     }
 
     @Override
@@ -44,6 +49,7 @@ public class ShieldBashEntity extends PersistentProjectileEntity {
         if (entityHitResult.getEntity() instanceof LivingEntity entity && !entity.getWorld().isClient()) {
             entity.addStatusEffect(new StatusEffectInstance(EffectRegistry.STUN_EFFECT, 20, 0, false, false, true));
             entity.playSound(SoundEvents.ITEM_SHIELD_BLOCK, 1, 0.8F);
+            entity.damage(this.getDamageSources().generic(), config.bash_damage);
             this.setRemoved(RemovalReason.DISCARDED); 
         } else if (entityHitResult.getEntity() instanceof ArrowEntity arrow && !arrow.getWorld().isClient()) {
             arrow.setVelocity(arrow.getVelocity().multiply(-1));
